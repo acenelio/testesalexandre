40cb6e08334ef44a9574ceddc1fba32943669324
Alexandre Pereira <apdo3939@hotmail.com>
2018-01-28 16:50:38 -0200

    Criado a Classe Posição da Camada Tabuleiro

---
+++ xadrez-console/tabuleiro/Posicao.cs
@@ -0,0 +1,23 @@
+﻿namespace tabuleiro
+{
+    class Posicao
+    {
+        //atributos
+        public int linha { get; set; }
+        public int coluna { get; set; }
+
+        //Construtor
+        public Posicao(int linha, int coluna)
+        {
+            this.linha = linha;
+            this.coluna = coluna;
+        }
+        //metodo to string
+        public override string ToString()
+        {
+            return  linha
+                + ", "
+                + coluna;
+        }
+    }
+}
